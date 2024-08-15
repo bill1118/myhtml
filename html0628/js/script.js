8e8c4b53de7d1bb797f405edf2847f38bf69f979
bill1118 <bill1118921@gmail.com>
2024-08-15 15:01:45 +0800

    2024

---
+++ html0628/js/script.js
@@ -0,0 +1,36 @@
+// 移除#about的背景影片
+$(function () {
+    if ($(window).width() < 821) {
+        $('#about video').remove();
+    }
+
+    /* 交替漢堡按鈕 */
+    $('.hamburger').click(function () {
+        /* hamburger 交替切換 .is-active */
+        $(this).toggleClass('is-active');
+        /* .navigation 交替切換 .show */
+        $('.navigation').toggleClass('show');
+    });
+
+    /* 滑動至指定位置 */
+    $('.menu a').click(function () {
+        let btn = $(this).attr('href');/* 返回取得屬性與值 */
+        let pos = $(btn).offset();/* 抓與相對的座標位置 */
+        $('html,body').animate({ scrollTop: pos.top }), 1000;
+    });
+
+    /* 滑動至頂 */
+    $('#gotop').click(function () {
+        $('html,body').animate({ scrollTop: 0 }), 1000;
+    });
+
+    // 至頂按鈕淡出淡入
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('#gotop').stop().fadeTo('', 1);
+        } else {
+            $('#gotop').stop().fadeOut();
+        }
+    });
+
+});
